67fc14d678dee2f285baba9b1fc147873399f2f4
xiaoju <764100457@qq.com>
2023-10-03 09:01:08 +0800

    feat(): 优化项目结构

--- _example/vue3.x/src/pages/DashBoard.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div></div>
-</template>
-
-<script lang="ts" setup name="Dashboard"></script>
-
-<style lang="scss" scoped></style>
--- _example/vue3.x/src/plugins/components.ts
+++
@@ -1,9 +0,0 @@
-import { App } from 'vue';
-
-import TreeTable from '@packages/utils-common/components/TreeTable/tableIndex.vue';
-import spaceIndex from '@packages/utils-common/components/Space/spaceIndex.vue';
-
-export default (instance: App) => {
-  instance.component('TreeTable', TreeTable);
-  instance.component('SpaceIndex', spaceIndex);
-};
